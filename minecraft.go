dc29e356ff042b6695633dbd4aff8c13beb519eb
Michael Woolnough <michael.woolnough@gmail.com>
2013-08-16 11:05:48 +0100

    Moved error types to main minecraft file

--- minecraft.go
+++ minecraft.go
@@ -25,6 +25,29 @@
 // Minecraft will be a full featured minecraft level editor/viewer.
 package minecraft
 
+type MissingTagError struct {
+	tagName string
+}
+
+func (m MissingTagError) Error() string {
+	return fmt.Sprintf("minecraft: missing %q tag", m.tagName)
+}
+
+type WrongTypeError struct {
+	tagName        string
+	expecting, got nbt.TagId
+}
+
+func (w WrongTypeError) Error() string {
+	return fmt.Sprintf("minecraft: tag %q is of incorrect type, expecting %q, got %q", w.tagName, w.expecting, w.got)
+}
+
+type OOB struct{}
+
+func (o OOB) Error() string {
+	return "Received Out-of-bounds error"
+}
+
 var (
 	transparentBlocks []uint16
 	lightBlocks       map[uint8]uint8
