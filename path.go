58d65ea24544a7135c1856a6d19cea0550a9d4d6
Michael Woolnough <michael.woolnough@gmail.com>
2013-08-30 11:27:37 +0100

    completed path function and tests

--- path.go
+++ path.go
@@ -1,114 +1,350 @@
+// Copyright (c) 2013 - Michael Woolnough <michael.woolnough@gmail.com>
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package minecraft
 
 import (
+	"compress/gzip"
+	"compress/zlib"
+	"encoding/binary"
+	"fmt"
+	"github.com/MJKWoolnough/io-watcher"
+	"github.com/MJKWoolnough/memio"
+	"github.com/MJKWoolnough/minecraft/nbt"
 	"io"
 	"io/ioutil"
+	"os"
 	"path"
+	"regexp"
+	"sort"
+	"time"
 )
 
 var filename *regexp.Regexp
 
-type readSeekCloser interface {
-	io.ReadSeeker
-	io.Closer
-}
-
-type writeSeekCloser interface {
-	io.WriteCloser
-	io.Closer
-}
-
-// Path allows different types of data streams (directory, compressed file,
-// net conn, in memory buffer etc.) to be read from.
 type Path interface {
-	readRegion(x, z int32) readSeekCloser
-	writeRegion(x, z int32) writeSeekCloser
-	readLevelDat() (io.ReadCloser, error)
-	writeLevelDat() (io.WriteCloser, error)
+	// Returns a nil nbt.Tag when chunk does not exists
+	GetChunk(int32, int32) (nbt.Tag, error)
+	SetChunk(...nbt.Tag) error
+	RemoveChunk(int32, int32) error
+	ReadLevelDat() (nbt.Tag, error)
+	WriteLevelDat(nbt.Tag) error
 	GetRegions() [][2]int32
-	Lock()
+	// 	GetChunks(int32, int32) [][2]int32
 }
 
-type NoLock struct{}
+const (
+	GZip byte = 1
+	Zlib byte = 2
+)
 
-func (n NoLock) Error() string {
-	return "lost lock on files"
-}
-
-type path struct {
+type FilePath struct {
 	dirname string
 	lock    bool
 }
 
-// NewPath constructs a new directory based path to read from.
-func NewPath(dirname string) (Path, error) {
+// NewFilePath constructs a new directory based path to read from.
+func NewFilePath(dirname string) (*FilePath, error) {
 	dirname = path.Clean(dirname)
 	if err := os.MkdirAll(dirname, 0755); err != nil {
 		return nil, err
 	}
-	p := &path{
+	p := &FilePath{
 		dirname,
 		false,
 	}
 	p.Lock()
-	return p
+	return p, nil
 }
 
-func (p *path) readRegion(x, z int32) (readSeekCloser, error) {
+func (p *FilePath) GetChunk(x, z int32) (nbt.Tag, error) {
 	if !p.lock {
 		return nil, &NoLock{}
 	}
-	f, err := os.Open(path.Join(p.dirname, "region", fmt.Sprintf("r.%d.%d.mca", x, z)))
+	f, err := os.Open(path.Join(p.dirname, "region", fmt.Sprintf("r.%d.%d.mca", x>>5, z>>5)))
 	if os.IsNotExist(err) {
 		return nil, nil
 	}
-	return f, err
-}
-
-func (p *path) writeRegion(x, z int32) (writeSeekCloser, error) {
-	if !p.lock {
-		return nil, &NoLock{}
-	}
-	if err := os.MkdirAll(path.Join(p.dirname, "region"), 0755); err != nil {
+	defer f.Close()
+	pos := int64((z&31)<<5 | (x & 31))
+	if _, err = f.Seek(4*pos, os.SEEK_SET); err != nil {
 		return nil, err
 	}
-	return os.OpenFile(path.Join(p.dirname, "region", fmt.Sprintf("r.%d.%d.mca", x, z)), os.O_WRONLY|os.O_CREATE, 0666)
+	var locationSize uint32
+	if err = binary.Read(f, binary.BigEndian, &locationSize); err != nil {
+		return nil, err
+	} else if locationSize>>8 == 0 {
+		return nil, nil
+	} else if _, err = f.Seek(int64(locationSize>>8<<12), os.SEEK_SET); err != nil {
+		return nil, err
+	}
+	reader := io.LimitReader(f, int64(locationSize&255<<12))
+	var (
+		length      uint32
+		compression byte
+	)
+	if err = binary.Read(reader, binary.BigEndian, &length); err != nil {
+		return nil, err
+	}
+	reader = io.LimitReader(reader, int64(length))
+	if err = binary.Read(reader, binary.BigEndian, &compression); err != nil {
+		return nil, err
+	}
+	switch compression {
+	case GZip:
+		gReader, err := gzip.NewReader(reader)
+		if err != nil {
+			return nil, err
+		}
+		defer gReader.Close()
+		reader = gReader
+	case Zlib:
+		if reader, err = zlib.NewReader(reader); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, &UnknownCompression{compression}
+	}
+	data, _, err := nbt.ReadNBTFrom(reader)
+	return data, err
 }
 
-func (p *path) readLevelDat() (io.ReadCloser, error) {
+type rc struct {
+	pos int32
+	buf []byte
+}
+
+func (p *FilePath) SetChunk(data ...nbt.Tag) error {
+	if !p.lock {
+		return &NoLock{}
+	}
+	regions := make(map[uint64][]rc)
+	poses := make([]uint64, 0)
+	for _, d := range data {
+		x, z, err := chunkCoords(d)
+		if err != nil {
+			return err
+		}
+		pos := uint64(z)<<32 | uint64(uint32(x))
+		for _, p := range poses {
+			if p == pos {
+				return &ConflictError{x, z}
+			}
+		}
+		poses = append(poses, pos)
+		r := uint64(z>>5)<<32 | uint64(uint32(x>>5))
+		reg := rc{pos: (z&31)<<5 | (x & 31)}
+		zl := zlib.NewWriter(memio.Create(&reg.buf))
+		if _, err := d.WriteTo(zl); err != nil {
+			return err
+		}
+		zl.Close()
+		if regions[r] == nil {
+			regions[r] = []rc{reg}
+		} else {
+			regions[r] = append(regions[r], reg)
+		}
+	}
+	for rId, chunks := range regions {
+		if err := p.setChunks(int32(rId&0xffffffff), int32(rId>>32), chunks); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type sia []uint32
+
+func (s sia) Len() int {
+	return 1024
+}
+
+func (s sia) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sia) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (p *FilePath) setChunks(x, z int32, chunks []rc) error {
+	if err := os.MkdirAll(path.Join(p.dirname, "region"), 0755); err != nil {
+		return err
+	}
+	f, err := os.OpenFile(path.Join(p.dirname, "region", fmt.Sprintf("r.%d.%d.mca", x, z)), os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	var positions [1024]uint32
+	if err = binary.Read(f, binary.BigEndian, positions[:]); err != nil && err != io.EOF {
+		return err
+	}
+	var todoChunks []rc
+	for _, chunk := range chunks {
+		newSize := uint32(len(chunk.buf)+5) >> 12
+		if uint32(len(chunk.buf))&4095 > 0 {
+			newSize++
+		}
+		if positions[chunk.pos]&255 == newSize {
+			if _, err = f.Seek(4*int64(chunk.pos)+4096, os.SEEK_SET); err != nil { // Write the time, then the data
+				return err
+			} else if _, err = f.Seek(int64(positions[chunk.pos])>>8<<12, os.SEEK_SET); err != nil {
+				return err
+			} else if err = binary.Write(f, binary.BigEndian, uint32(len(chunk.buf))+1); err != nil {
+				return err
+			} else if err = binary.Write(f, binary.BigEndian, Zlib); err != nil {
+				return err
+			} else if err = binary.Write(f, binary.BigEndian, chunk.buf); err != nil {
+				return err
+			}
+		} else {
+			todoChunks = append(todoChunks, chunk)
+			positions[chunk.pos] = 0
+		}
+	}
+	for _, chunk := range todoChunks {
+		sort.Sort(sia(positions[:]))
+		newPosition := (positions[1023] >> 8) + (positions[1023] & 255)
+		if newPosition == 0 {
+			newPosition = 2
+		}
+		lastPos := uint32(2)
+		smallest := uint32(0xffffffff)
+		writeLastByte := true
+		newSize := uint32(len(chunk.buf) + 5)
+		if newSize&4095 > 0 {
+			newSize >>= 12
+			newSize++
+		} else {
+			newSize >>= 12
+		}
+		// Find earliest, closest match in size for least fragmentation.
+		for i := 0; i < 1024; i++ {
+			loc := positions[i] >> 8
+			if loc > 0 {
+				size := positions[i] & 255
+				if space := loc - lastPos; space >= newSize && space < smallest {
+					smallest = space
+					newPosition = lastPos
+					writeLastByte = false // by definition it has data that is after it now, so no need to make up to mod 4096 bytes
+				}
+				lastPos = loc + size
+			}
+		}
+		positions[0] = newPosition<<8 | newSize&255
+		// Write the new position
+		if _, err = f.Seek(4*int64(chunk.pos), os.SEEK_SET); err != nil {
+			return err
+		} else if err = binary.Write(f, binary.BigEndian, positions[0]); err != nil {
+			return err
+		} else if _, err = f.Seek(4*(int64(chunk.pos)+1024), os.SEEK_SET); err != nil { // Write the time, then the data
+			return err
+		} else if err = binary.Write(f, binary.BigEndian, uint32(time.Now().Unix())); err != nil {
+			return err
+		} else if _, err = f.Seek(int64(newPosition)<<12, os.SEEK_SET); err != nil {
+			return err
+		} else if err = binary.Write(f, binary.BigEndian, uint32(len(chunk.buf))+1); err != nil {
+			return err
+		} else if err = binary.Write(f, binary.BigEndian, Zlib); err != nil {
+			return err
+		} else if err = binary.Write(f, binary.BigEndian, chunk.buf); err != nil {
+			return err
+		} else if writeLastByte { // Make filesize mod 4096 (for minecraft compatibility)
+			if _, err = f.Seek(int64(newPosition+newSize)<<12-1, os.SEEK_SET); err != nil {
+				return err
+			} else if err = binary.Write(f, binary.BigEndian, byte(0)); err != nil {
+				return err
+			}
+
+		}
+	}
+	return nil
+}
+
+func (p *FilePath) RemoveChunk(x, z int32) error {
+	chunkX := x & 31
+	regionX := x >> 5
+	chunkZ := z & 31
+	regionZ := z >> 5
+	f, err := os.OpenFile(path.Join(p.dirname, "region", fmt.Sprintf("r.%d.%d.mca", regionX, regionZ)), os.O_WRONLY, 0666)
+	if os.IsNotExist(err) {
+		return nil
+	} else if err != nil {
+		return err
+	}
+	defer f.Close()
+	if _, err = f.Seek(int64(chunkZ<<5|chunkX)*4, os.SEEK_SET); err != nil {
+		return err
+	}
+	return binary.Write(f, binary.BigEndian, int32(0))
+}
+
+func (p *FilePath) ReadLevelDat() (nbt.Tag, error) {
 	if !p.lock {
 		return nil, &NoLock{}
 	}
 	f, err := os.Open(path.Join(p.dirname, "level.dat"))
 	if os.IsNotExist(err) {
 		return nil, nil
+	} else if err != nil {
+		return nil, err
 	}
-	return f, err
+	defer f.Close()
+	g, err := gzip.NewReader(f)
+	if err != nil {
+		return nil, err
+	}
+	data, _, err := nbt.ReadNBTFrom(g)
+	return data, err
 }
 
-func (p *path) writeLevelDat() (io.WriteCloser, error) {
+func (p *FilePath) WriteLevelDat(data nbt.Tag) error {
 	if !p.lock {
-		return nil, &NoLock{}
+		return &NoLock{}
 	}
-	os.OpenFile(path.Join(p.dirname, "level.dat"), os.O_WRONLY|os.O_CREATE, 0666)
-}
-
-// Update tracks the lock file for updates to remove the lock.
-func (p *path) Update(filname string, mode uint8) {
-	p.lock = false
-	watcher.RemoveWatcher(p.dirname)
+	f, err := os.OpenFile(path.Join(p.dirname, "level.dat"), os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return nil
+	}
+	defer f.Close()
+	g := gzip.NewWriter(f)
+	defer g.Close()
+	_, err = data.WriteTo(g)
+	return err
 }
 
 // GetRegions returns a list of region x,z coords of all generated regions.
-func (p path) GetRegions() [][2]int32 {
-	files := ioutil.ReadDir(path.Join(p.dirname, "region"))
+func (p *FilePath) GetRegions() [][2]int32 {
+	files, _ := ioutil.ReadDir(path.Join(p.dirname, "region"))
 	toRet := make([][2]int32, 0)
 	var x, z int32
 	for _, file := range files {
 		if !file.IsDir() {
 			if nums := filename.FindStringSubmatch(file.Name()); nums != nil {
-				fmt.Sscan(nums[0], &x)
-				fmt.Sscan(nums[1], &z)
+				fmt.Sscan(nums[1], &x)
+				fmt.Sscan(nums[2], &z)
 				toRet = append(toRet, [2]int32{x, z})
 			}
 		}
@@ -116,19 +352,143 @@ func (p path) GetRegions() [][2]int32 {
 	return toRet
 }
 
+// Update tracks the lock file for updates to remove the lock.
+func (p *FilePath) Update(filname string, mode uint8) {
+	p.lock = false
+	watcher.StopWatch(p.dirname)
+}
+
 // Lock will retake the lock file if it has been lost. May cause corruption.
-func (p *path) Lock() {
+func (p *FilePath) Lock() {
 	if p.lock {
 		return
 	}
 	session := path.Join(p.dirname, "session.lock")
 	if f, err := os.Create(session); err != nil {
-		return nil, err
+		return //??
 	} else {
 		fmt.Fprintf(f, "%d", timestampMS())
 		f.Close()
 	}
-	watcher.Watch(session, l)
+	watcher.Watch(session, p)
+	p.lock = true
+}
+
+func (p *FilePath) Defrag(x, z int32) error {
+	return nil
+}
+
+type memPath struct {
+	level  []byte
+	chunks map[uint64][]byte
+}
+
+func NewMemPath() *memPath {
+	return &memPath{chunks: make(map[uint64][]byte)}
+}
+
+func (m *memPath) GetChunk(x, z int32) (nbt.Tag, error) {
+	pos := uint64(z)<<32 | uint64(uint32(x))
+	if m.chunks[pos] == nil {
+		return nil, nil
+	}
+	return m.read(m.chunks[pos])
+}
+
+func (m *memPath) SetChunk(data ...nbt.Tag) error {
+	for _, d := range data {
+		x, z, err := chunkCoords(d)
+		if err != nil {
+			return err
+		}
+		var buf []byte
+		if err = m.write(d, &buf); err != nil {
+			return err
+		}
+		pos := uint64(z)<<32 | uint64(uint32(x))
+		m.chunks[pos] = buf
+	}
+	return nil
+}
+
+func (m *memPath) RemoveChunk(x, z int32) error {
+	pos := uint64(z)<<32 | uint64(uint32(x))
+	delete(m.chunks, pos)
+	return nil
+}
+
+func (m *memPath) ReadLevelDat() (nbt.Tag, error) {
+	return m.read(m.level)
+}
+
+func (m *memPath) WriteLevelDat(data nbt.Tag) error {
+	return m.write(data, &m.level)
+}
+
+func (m *memPath) GetRegions() [][2]int32 {
+	toRet := make([][2]int32, 0)
+JP:
+	for i := range m.chunks {
+		x := int32(i&0xffffffff) >> 5
+		z := int32(i>>32) >> 5
+		for _, j := range toRet {
+			if j[0] == x && j[1] == z {
+				continue JP
+			}
+		}
+		toRet = append(toRet, [2]int32{x, z})
+	}
+	return toRet
+}
+
+func (m *memPath) read(buf []byte) (nbt.Tag, error) {
+	z, err := zlib.NewReader(memio.Open(buf))
+	if err != nil {
+		return nil, err
+	}
+	data, _, err := nbt.ReadNBTFrom(z)
+	return data, err
+}
+
+func (m *memPath) write(data nbt.Tag, buf *[]byte) error {
+	z := zlib.NewWriter(memio.Create(buf))
+	defer z.Close()
+	_, err := data.WriteTo(z)
+	return err
+}
+
+func chunkCoords(data nbt.Tag) (x int32, z int32, err error) {
+	if data.TagId() != nbt.Tag_Compound {
+		err = &WrongTypeError{"[Chunk Base]", nbt.Tag_Compound, data.TagId()}
+		return
+	} else if lTag := data.Data().(*nbt.Compound).Get("Level"); lTag == nil {
+		err = &MissingTagError{"[Chunk Base]->Level"}
+		return
+	} else if lTag.TagId() != nbt.Tag_Compound {
+		err = &WrongTypeError{"[Chunk Base]->Level", nbt.Tag_Compound, lTag.TagId()}
+		return
+	} else {
+		lCmp := lTag.Data().(*nbt.Compound)
+		if xPos := lCmp.Get("xPos"); xPos == nil {
+			err = &MissingTagError{"[Chunk Base]->Level->xPos"}
+			return
+		} else if xPos.TagId() != nbt.Tag_Int {
+			err = &WrongTypeError{"[Chunk Base]->Level->xPos", nbt.Tag_Int, xPos.TagId()}
+			return
+		} else {
+			x = int32(*xPos.Data().(*nbt.Int))
+		}
+		if zPos := lCmp.Get("zPos"); zPos == nil {
+			err = &MissingTagError{"[Chunk Base]->Level->zPos"}
+			return
+		} else if zPos.TagId() != nbt.Tag_Int {
+			err = &WrongTypeError{"[Chunk Base]->Level->zPos", nbt.Tag_Int, zPos.TagId()}
+			return
+		} else {
+			z = int32(*zPos.Data().(*nbt.Int))
+		}
+	}
+	return
 }
 
 func init() {
---
+++ path_test.go
@@ -0,0 +1,278 @@
+package minecraft
+
+import (
+	"encoding/binary"
+	"fmt"
+	"github.com/MJKWoolnough/minecraft/nbt"
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+)
+
+func testPathChunkSetGet(t *testing.T, path Path) {
+	toPlace := [][]nbt.Tag{
+		[]nbt.Tag{
+			addPos(0, 0, 3),  //0[4]
+			addPos(1, 0, 2),  //0[4],1[3]
+			addPos(2, 0, 1),  //0[4],1[3],2[2]
+			addPos(3, 0, 0),  //0[4],1[3],2[2],3[1]
+			addPos(20, 0, 2), //0[4],1[3],2[2],3[1],20[3]
+			addPos(0, 20, 1), //0[4],1[3],2[2],3[1],20[3],640[2]
+			addPos(-1, 0, 0),
+			addPos(0, -1, 0),
+			addPos(-1, -1, 1),
+			addPos(-3, -3, 1),
+		},
+		[]nbt.Tag{
+			addPos(0, 0, 1), //0[2],[2],1[3],2[2],3[1],20[3],640[2]
+			addPos(1, 0, 2), //0[2],[2],1[3],2[2],3[1],20[3],640[2]
+			addPos(3, 0, 1), //0[2],3[2],1[3],2[2],[1],20[3],640[2]
+			addPos(4, 0, 0), //0[2],3[2],1[3],2[2],4[1],(!)20[3],(!)640[2] | (2 + 0) << 8 | 2, (2 + 2) << 8 | 2,(2 + 4) << 8 | 3,(2 + 7) << 8 | 2, (2 + 9) << 8 | 1
+			addPos(-1, 0, 1),
+			addPos(0, -2, 1),
+			addPos(-1, -1, 0),
+			addPos(-3, -3, 0),
+		},
+	}
+	retest := []bool{
+		false,
+		false,
+		true,
+		false,
+		true,
+		true,
+		false,
+		true,
+		false,
+		false,
+		true,
+		true,
+		true,
+		true,
+		true,
+		true,
+		true,
+		true,
+	}
+	for num, chunkList := range toPlace {
+		if err := path.SetChunk(chunkList...); err != nil {
+			t.Fatal(err.Error())
+		}
+		for i, chunk := range chunkList {
+			if x, z, err := chunkCoords(chunk); err != nil {
+				t.Fatal(err.Error())
+			} else if thatChunk, err := path.GetChunk(x, z); err != nil {
+				t.Fatal(err.Error())
+			} else if thatChunk == nil {
+				t.Fatalf("testPathChunkSetGet: 0-%d-%d: no chunk returned", num, i)
+			} else if !thatChunk.Equal(chunk) {
+				t.Fatalf("testPathChunkSetGet: 0-%d-%d: returned chunk not equal to set chunk, expecting: -\n%s\ngot: -\n%s", num, i, chunk.String(), thatChunk.String())
+			}
+		}
+	}
+	for num, chunkList := range toPlace {
+		for i, chunk := range chunkList {
+			if x, z, err := chunkCoords(chunk); err != nil {
+				t.Fatal(err.Error())
+			} else if thatChunk, err := path.GetChunk(x, z); err != nil {
+				t.Fatal(err.Error())
+			} else if thatChunk == nil {
+				t.Fatalf("testPathChunkSetGet: 1-%d-%d: no chunk returned", num, i)
+			} else if thatChunk.Equal(chunk) != retest[0] {
+				t.Errorf("testPathChunkSetGet: 1-%d-%d: returned chunk not equal to set chunk, expecting: -\n%s\ngot: -\n%s", num, i, chunk.String(), thatChunk.String())
+			}
+			retest = retest[1:]
+		}
+	}
+}
+
+func testPathLevelSetGet(t *testing.T, path Path) {
+	levelDat := nbt.NewTag("", nbt.NewCompound([]nbt.Tag{
+		nbt.NewTag("Beep", nbt.NewCompound([]nbt.Tag{
+			nbt.NewTag("SomeInt", nbt.NewInt(45)),
+			nbt.NewTag("SomeString", nbt.NewString("hello")),
+		})),
+	}))
+	if err := path.WriteLevelDat(levelDat); err != nil {
+		t.Error(err.Error())
+	} else if newLevelDat, err := path.ReadLevelDat(); err != nil {
+		t.Error(err.Error())
+	} else if !newLevelDat.Equal(levelDat) {
+		t.Errorf("level data doesn't match original, expecting: -\n%s\ngot: -\n%s", levelDat.String(), newLevelDat.String())
+	}
+}
+
+func testPathChunkRemove(t *testing.T, path Path) {
+	toRemove := [][2]int32{
+		{0, 20},
+		{20, 0},
+		{-3, -3},
+	}
+	for num, tR := range toRemove {
+		if err := path.RemoveChunk(tR[0], tR[1]); err != nil {
+			t.Error(err.Error())
+		} else if tC, err := path.GetChunk(tR[0], tR[1]); err != nil {
+			t.Error(err.Error())
+		} else if tC != nil {
+			t.Errorf("testPathChunkRemove %d: failed to remove chunk at %d,%d", num, tR[0], tR[1])
+		}
+	}
+}
+
+func testPathRegionsGet(t *testing.T, path Path) {
+	regions := path.GetRegions()
+	should := [][2]int32{
+		{-1, -1},
+		{0, -1},
+		{-1, 0},
+		{0, 0},
+	}
+	if len(regions) != len(should) {
+		t.Error("returned regions slice does not match expected")
+	} else {
+	CL:
+		for i := 0; i < len(regions); i++ {
+			for j := 0; j < len(should); j++ {
+				if regions[i][0] == should[j][0] && regions[i][1] == should[j][1] {
+					should = append(should[:j], should[j+1:]...)
+					continue CL
+				}
+			}
+			t.Error("returned regions slice does not match expected")
+			break
+		}
+	}
+}
+
+func addPos(x, z int32, chunkNum uint8) nbt.Tag {
+	e := chunks[chunkNum].data
+	e.Set(nbt.NewTag("xPos", nbt.NewInt(x)))
+	e.Set(nbt.NewTag("zPos", nbt.NewInt(z)))
+	return chunks[chunkNum].GetNBT()
+}
+
+func TestMemPath(t *testing.T) {
+	f := NewMemPath()
+	if a := len(f.GetRegions()); a != 0 {
+		t.Errorf("should start with zero regions, have %d", a)
+		return
+	}
+	testPathChunkSetGet(t, f)
+	testPathLevelSetGet(t, f)
+	testPathRegionsGet(t, f)
+	testPathChunkRemove(t, f)
+}
+
+func TestFilePath(t *testing.T) {
+	var (
+		tempDir string
+		err     error
+		f       Path
+	)
+	if tempDir, err = ioutil.TempDir("", "minecraft-path-test"); err != nil {
+		t.Error(err.Error())
+	} else if f, err = NewFilePath(tempDir); err != nil {
+		t.Error(err.Error())
+	}
+	if a := len(f.GetRegions()); a != 0 {
+		t.Errorf("should start with zero regions, have %d", a)
+		return
+	}
+	testPathChunkSetGet(t, f)
+	testPathLevelSetGet(t, f)
+	testPathRegionsGet(t, f)
+	testPathChunkRemove(t, f)
+
+	//Check Files
+	file, err := os.Open(path.Join(tempDir, "region", "r.0.0.mca"))
+	if err != nil {
+		t.Error(err.Error())
+	}
+	var positions, should [1024]uint32
+	if err = binary.Read(file, binary.BigEndian, positions[:]); err != nil {
+		t.Error(err.Error())
+	}
+	file.Close()
+	should[0] = (2+0)<<8 | 2 //pos 0 + offset(2), size 2
+	should[1] = (2+4)<<8 | 3 //pos 4 + offset(2), size 3
+	should[2] = (2+7)<<8 | 2 //pos 7 + offset(2), size 2
+	should[3] = (2+2)<<8 | 2 //pos 2 + offset(2), size 2
+	should[4] = (2+9)<<8 | 1 //pos 9 + offset(2), size 1
+
+	for i := 0; i < 1024; i++ {
+		if should[i] != positions[i] {
+			t.Errorf("chunk position/size incorrect, expecting chunk %d at %d, got %d", i, should[i], positions[i])
+			break
+		}
+	}
+	regions := f.GetRegions()
+
+	for _, region := range regions {
+		if fi, err := os.Stat(path.Join(tempDir, "region", fmt.Sprintf("r.%d.%d.mca", region[0], region[1]))); err != nil {
+			t.Error(err.Error())
+		} else if s := fi.Size(); s%4096 != 0 {
+			t.Errorf("regions %d,%d filesize not divisible by 4096, got %d", region[0], region[1], s)
+		}
+	}
+
+	if err = os.RemoveAll(tempDir); err != nil {
+		t.Error(err.Error())
+	}
+}
+
+var chunks [4]chunk
+
+func init() {
+	a, _ := newChunk(0, 0, nil)
+	b, _ := newChunk(0, 0, nil)
+	c, _ := newChunk(0, 0, nil)
+	d, _ := newChunk(0, 0, nil)
+	chunks = [4]chunk{
+		*a,
+		*b,
+		*c,
+		*d,
+	}
+	for i := 0; i < 16; i++ {
+		for j := 0; j < 16; j++ {
+			for k := 0; k < 256; k++ {
+				if k < 8 {
+					chunks[3].SetBlock(int32(i), int32(k), int32(j), &Block{
+						uint16(i+j+k) % 4096,
+						uint8(i),
+						[]nbt.Tag{
+							nbt.NewTag("testMD", nbt.NewInt(int32(i*j*k))),
+						},
+						k%2 == 0,
+					})
+				}
+				if k < 250 {
+					chunks[2].SetBlock(int32(i), int32(k), int32(j), &Block{1, 0, nil, false})
+				} else {
+					chunks[2].SetBlock(int32(i), int32(k), int32(j), &Block{
+						1,
+						0,
+						[]nbt.Tag{
+							nbt.NewTag("testMD", nbt.NewInt(int32(i*j*k))),
+						},
+						false,
+					})
+				}
+			}
+			chunks[1].SetBlock(int32(i), int32(j)*16, int32(j), &Block{
+				uint16(i*j + i + j),
+				uint8(i),
+				[]nbt.Tag{
+					nbt.NewTag("testMD1", nbt.NewInt(int32(i))),
+					nbt.NewTag("testMD2", nbt.NewInt(int32(i+1))),
+					nbt.NewTag("testMD3", nbt.NewInt(int32(i+2))),
+					nbt.NewTag("testMD4", nbt.NewInt(int32(i+3))),
+					nbt.NewTag("testMD5", nbt.NewInt(int32(i+4))),
+				},
+				true,
+			})
+		}
+		chunks[0].SetBlock(int32(i), int32(i), int32(i), &Block{uint16(i), uint8(i), nil, false})
+	}
+}
