1cd235a912bf49b7aa0c4cc4c403ab302dc56cbe
Michael Woolnough <michael.woolnough@gmail.com>
2015-06-01 12:49:40 +0100

    Added minecraft map support for stdlib/image

---
+++ maps/decode.go
@@ -0,0 +1,79 @@
+package maps
+
+import (
+	"image"
+	"io"
+
+	"github.com/MJKWoolnough/minecraft/nbt"
+)
+
+func init() {
+	image.RegisterFormat("minecraftmap", "\x0a\x00\x00\x0a\x00\x04data", Decode, Config)
+}
+
+func readData(r io.Reader) (nbt.Compound, error) {
+	t, err := nbt.Decode(r)
+	if err != nil {
+		return nil, err
+	}
+
+	if t.TagID() != nbt.TagCompound {
+		return nil, nbt.WrongTag{nbt.TagCompound, t.TagID()}
+	}
+
+	d := t.Data().(nbt.Compound).Get("data")
+	if d.TagID() != nbt.TagCompound {
+		return nil, nbt.WrongTag{nbt.TagCompound, d.TagID()}
+	}
+	return d.Data().(nbt.Compound), nil
+}
+
+func getDimensions(d nbt.Compound) image.Rectangle {
+	var width, height int
+	w := d.Get("width")
+	if w.TagID() == nbt.TagShort {
+		width = int(w.Data().(nbt.Short))
+	}
+	h := d.Get("height")
+	if h.TagID() == nbt.TagShort {
+		height = int(h.Data().(nbt.Short))
+	}
+	return image.Rectangle{
+		image.Point{0, 0},
+		image.Point{width, height},
+	}
+}
+
+func Decode(r io.Reader) (image.Image, error) {
+	d, err := readData(r)
+	if err != nil {
+		return nil, err
+	}
+
+	c := d.Get("colors")
+	if c.TagID() != nbt.TagByteArray {
+		return nil, nbt.WrongTag{nbt.TagByteArray, c.TagID()}
+	}
+
+	rect := getDimensions(d)
+
+	return &image.Paletted{
+		Pix:     c.Data().(nbt.ByteArray).Bytes(),
+		Stride:  rect.Max.X,
+		Rect:    rect,
+		Palette: palette,
+	}, nil
+}
+
+func Config(r io.Reader) (image.Config, error) {
+	d, err := readData(r)
+	if err != nil {
+		return image.Config{}, err
+	}
+	rect := getDimensions(d)
+	return image.Config{
+		ColorModel: palette,
+		Width:      rect.Max.X,
+		Height:     rect.Max.Y,
+	}, nil
+}
---
+++ maps/encode.go
@@ -0,0 +1,53 @@
+package maps
+
+import (
+	"errors"
+	"image"
+	"io"
+
+	"github.com/MJKWoolnough/minecraft/nbt"
+)
+
+func Encode(w io.Writer, i image.Image) error {
+	e := Encoder{
+		Dimension: -128,
+		Scale:     3,
+	}
+	return e.Encode(w, i)
+}
+
+type Encoder struct {
+	Scale, Dimension int8
+	CenterX, CenterZ int32
+}
+
+func (e *Encoder) Encode(w io.Writer, im image.Image) error {
+	width := im.Bounds().Dx()
+	height := im.Bounds().Dy()
+	if width > 0xffff || height > 0xffff || width < 0 || height < 0 {
+		return InvalidDimensions
+	}
+
+	colours := make(nbt.ByteArray, 0, width*height)
+	for j := im.Bounds().Min.Y; j < im.Bounds().Max.Y; j++ {
+		for i := im.Bounds().Min.X; i < im.Bounds().Max.X; i++ {
+			colours = append(colours, int8(palette.Index(im.At(i, j))))
+		}
+	}
+
+	return nbt.Encode(w, nbt.NewTag("", nbt.Compound{
+		nbt.NewTag("data", nbt.Compound{
+			nbt.NewTag("scale", nbt.Byte(e.Scale)),
+			nbt.NewTag("dimension", nbt.Byte(e.Dimension)),
+			nbt.NewTag("height", nbt.Short(height)),
+			nbt.NewTag("width", nbt.Short(width)),
+			nbt.NewTag("xCenter", nbt.Int(e.CenterX)),
+			nbt.NewTag("zCenter", nbt.Int(e.CenterZ)),
+			nbt.NewTag("colors", colours),
+		}),
+	}))
+}
+
+// Errors
+
+var InvalidDimensions = errors.New("cannot encode an image with the given dimensions")
