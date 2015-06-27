39fceb79b4875a1a06f6f07663801702ed290da1
Michael Woolnough <michael.woolnough@gmail.com>
2015-06-27 13:58:10 +0100

    started generator package

---
+++ generators/generator.go
@@ -0,0 +1,19 @@
+package generator
+
+import "github.com/MJKWoolnough/minecraft"
+
+type Generator struct {
+	*minecraft.Level
+}
+
+func New(l *minecraft.Level) Generator {
+	return Generator{l}
+}
+
+func NewFromPath(p minecraft.Path) (Generator, error) {
+	l, err := minecraft.NewLevel(p)
+	if err != nil {
+		return Generator{}, err
+	}
+	return Generator{l}, nil
+}
