7b90d7202ffd9987f409f6452b2d86a278c0061a
Michael Woolnough <michael.woolnough@gmail.com>
2015-07-12 19:05:33 +0100

    Added MirrorX test

--- manipulators/area_test.go
+++ manipulators/area_test.go
@@ -468,3 +468,67 @@ func TestAreaRotate270(t *testing.T) {
 		return
 	}
 }
+
+func TestAreaMirrorX(t *testing.T) {
+	t.Parallel()
+	l, _ := minecraft.NewLevel(minecraft.NewMemPath())
+	defer l.Close()
+	a := NewArea(0, 0, 0, 2, 0, 2, l)
+	b := NewArea(3, 0, 0, 5, 0, 2, l)
+	numToArea(a,
+		0, 1, 2,
+		3, 4, 5,
+		6, 7, 8,
+	)
+	a.MirrorX()
+	numToArea(b,
+		2, 1, 0,
+		5, 4, 3,
+		8, 7, 6,
+	)
+	if !a.EqualTo(b) {
+		t.Errorf("1: single X Mirror failed")
+		return
+	}
+	a.MirrorX()
+	numToArea(b,
+		0, 1, 2,
+		3, 4, 5,
+		6, 7, 8,
+	)
+	if !a.EqualTo(b) {
+		t.Errorf("1: double X Mirror failed")
+		return
+	}
+
+	a = NewArea(0, 0, 0, 3, 0, 3, l)
+	b = NewArea(4, 0, 0, 7, 0, 3, l)
+	numToArea(a,
+		0, 1, 2, 3,
+		4, 5, 6, 7,
+		8, 9, 10, 11,
+		12, 13, 14, 15,
+	)
+	a.MirrorX()
+	numToArea(b,
+		3, 2, 1, 0,
+		7, 6, 5, 4,
+		11, 10, 9, 8,
+		15, 14, 13, 12,
+	)
+	if !a.EqualTo(b) {
+		t.Errorf("2: single X Mirror failed")
+		return
+	}
+	a.MirrorX()
+	numToArea(b,
+		0, 1, 2, 3,
+		4, 5, 6, 7,
+		8, 9, 10, 11,
+		12, 13, 14, 15,
+	)
+	if !a.EqualTo(b) {
+		t.Errorf("2: double X Mirror failed")
+		return
+	}
+}
