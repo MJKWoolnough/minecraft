4e06eb75c98fcd84f9d14524a5c0fed1f1cf4635
Michael Woolnough <michael.woolnough@gmail.com>
2015-07-16 18:30:42 +0100

    Simplified options

--- generators/template.go
+++ generators/template.go
@@ -21,52 +21,30 @@ type options struct {
 	Rotation, Mirror, BlockSwapper func(minecraft.Block) minecraft.Block
 }
 
-func (o *options) Rotate90() {
-	o.Rotation = manipulators.Rotate90
-}
-
-func (o *options) Rotate180() {
-	o.Rotation = manipulators.Rotate180
-}
-
-func (o *options) Rotate270() {
-	o.Rotation = manipulators.Rotate270
-}
-
-func (o *options) MirrorX() {
-	o.Mirror = manipulators.MirrorX
-}
-
-func (o *options) MirrorZ() {
-	o.Mirror = manipulators.MirrorZ
-}
-
 type Modifier func(*options)
 
 func Rotate90() Modifier {
-	return options.Rotate90
+	return func(o *options) { o.Rotation = manipulators.Rotate90 }
 }
 
 func Rotate180() Modifier {
-	return options.Rotate180
+	return func(o *options) { o.Rotation = manipulators.Rotate180 }
 }
 
 func Rotate270() Modifier {
-	return options.Rotate270
+	return func(o *options) { o.Rotation = manipulators.Rotate270 }
 }
 
 func MirrorX() Modifier {
-	return options.MirrorX
+	return func(o *options) { o.MirrorX = manipulators.MirrorX }
 }
 
 func MirrorZ() Modifier {
-	return options.MirrorZ
+	return func(o *options) { o.MirrorZ = manipulators.MirrorZ }
 }
 
 func BlockSwapper(bs func(minecraft.Block) minecraft.Block) Modifier {
-	return func(o *options) {
-		o.BlockSwapper = bs
-	}
+	return func(o *options) { o.BlockSwapper = bs }
 }
 
 func noop(b minecraft.Block) minecraft.Block {
